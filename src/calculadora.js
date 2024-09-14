baeb6ecc1da9e4ce302a40eb2eeb737dfd7bb64d
Miguel Soares <miguelsoares01@gmail.com>
2024-09-14 16:06:42 +0000

    4 commmit adicao codigo multiplicação calculadora develop

--- src/calculadora.js
+++ src/calculadora.js
@@ -9,3 +9,14 @@ module.exports = {
 add,
 subtract
 };
+
+// Função de multiplicação
+function multiply(a, b) {
+return a * b;
+}
+// Exportar as funções
+module.exports = {
+add,
+subtract,
+multiply
+};
