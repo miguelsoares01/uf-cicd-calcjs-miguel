1022a1f30418235d5a6dee12f8415c9e822be8f0
Miguel Soares <miguelsoares01@gmail.com>
2024-09-14 16:08:34 +0000

    5 commmit adicao codigo divisao calculadora develop

--- src/calculadora.js
+++ src/calculadora.js
@@ -20,3 +20,21 @@ add,
 subtract,
 multiply
 };
+
+// Função de divisão
+function divide(a, b) {
+if (b === 0) {
+throw new Error("Divisão por zero não é permitida.");
+}
+return a / b;
+}
+// Exportar as funções
+module.exports = {
+add,
+subtract,
+divide,
+};
+
+
+
+
